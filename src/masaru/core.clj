bb12e24aeb62e879f39fdb6c0904aa6acbc8c6d9
Ysmiraak <ysmiraak@gmail.com>
2016-01-01 02:58:35 +0100

    Almost working now.

--- src/masaru/core.clj
+++ src/masaru/core.clj
@@ -1,44 +1,56 @@
 (ns masaru.core)
 
-(defrecord Vertex [predecs symbol datas])
-(defrecord Predec [state vertex])
+(defrecord Vertex [symbol data? prevmap])
 
 (defn consume
   "Automaton A consumes symbol S at vertex V with method M."
   [A S V M]
-  (letfn [(process [v] (->> v (get-actions S) (map (partial act v)) join))
-          (get-actions [s v] ; get all possible actions at vertex v with symbol s
-            (->> v :predecs (map #((A (:state %)) s)) (reduce into)))
-          (act [v a] ; perform action a (either a number or a vector) at vertex v
+  (letfn [(process [s v]
+            (->> v :prevmap keys
+                 (map #((A %) s))
+                 (reduce into)
+                 ;; returns the set of all possible actions at vertex v with symbol s
+                 (map (partial act v))
+                 join))
+          
+          (get-predecs [v] (reduce into (vals (:prevmap v))))
+
+          (act [v a]
+            ;; perform action a (either a number or a vector) at vertex v
             (if (number? a) ; a number represents the state for a shift operation
-              (->Vertex #{(->Predec a v)} S #{}) ; a vector is a reduction rule
-              (loop [a (pop a) s (peek a) ps (:predecs v) ds [(list v)]]
+              (->Vertex S nil {a #{v}}) ; a vector is a reduction rule
+              (loop [a (pop a) dvs [(list v)]]
                 (if (= 1 (count a))
-                  (-> #(->> %2 :vertex
-                            (get-actions s)
-                            (map (fn [goto] (->Predec goto (:vertex %2))))
-                            (into %1))
-                      (reduce #{} ps)
-                      (->Vertex s (reduce #(conj %1 (apply M %2)) #{} ds))
-                      process)
-                  (let [vs (filter #(= s (:symbol %)) (map :vertex ps)) a (pop a)]
-                    (recur a (peek a)
-                           (reduce into (map :predecs vs))
-                           (for [v vs d ds] (conj d v))))))))
+                  (->> dvs
+                       (mapcat
+                        (fn [dv]
+                          (->> (first dv)
+                               get-predecs
+                               (map (fn [v]
+                                      (->> v :prevmap keys
+                                           (map #((A %) (peek a)))
+                                           (reduce #(assoc %1 %2 #{v}) {})
+                                           (->Vertex (peek a) (M dv))))))))
+                       (map (partial process S))
+                       join)
+                  (recur (pop a)
+                         (for [dv dvs p (get-predecs (first dv))]
+                           (conj dv p)))))))
           (join [vs] ; merge vertices created through all courses of actions
             (case (count vs) 0 nil 1 (first vs)
-                  (->Vertex (reduce into (map :predecs vs))
-                            S (reduce into (map :datas vs)))))]
-    (process V)))
+                  (->Vertex (:symbol (first vs))
+                            (reduce into (map :datas vs))
+                            (apply merge-with into (map :prevmap vs)))))]
+    (process S V)))
 
-;; NOTE on M: It must accept a set and a list of vertices as arguments,
-;; and return a set. E.g. (fn do-nothing [ds vs] ds)
+;; NOTE on M: It must accept a list of vertices as arguments,
+;; and return a set. E.g. (fn do-nothing [dv] #{})
 
 (defn parse
   "Let states consume string with method. Returns the final vertex
   whose predecs are the possible parses, or nil if no parse found."
   [states method string]
-  (loop [v (->Vertex #{(->Predec 0 nil)} nil #{}) string string]
+  (loop [v (->Vertex nil nil {0 #{}}) string string]
     (when-not (nil? v)
       (if (empty? string)
         (consume states :$ v method)
@@ -47,18 +59,18 @@
 (defn parse-forest
   "Let states consumes string for building a parse forest."
   [states string]
-  (parse states list string))
+  (parse states (fn [dv] #{dv}) string))
 
 (defn draw-forest-as-sexp
   "Draw forest from vertex v as s-expression, where the or-nodes are
   represented as vectos."
   [v]
   (if (= :$ (:symbol v))
-    (->> v :predecs (map :vertex) (mapv draw-forest-as-sexp))
-    (case (count (:datas v))
+    (->> v :prevmap vals (reduce into) (mapv draw-forest-as-sexp))
+    (case (count (:data? v))
       0 (:symbol v)
-      1 (conj (apply map draw-forest-as-sexp (:datas v)) (:symbol v))
-      (mapv #(conj (map draw-forest-as-sexp %) (:symbol v)) (:datas v)))))
+      1 (conj (apply map draw-forest-as-sexp (:data? v)) (:symbol v))
+      (mapv #(conj (map draw-forest-as-sexp %) (:symbol v)) (:data? v)))))
 
 (defn draw-forest-as-graph
   "todo"
--- src/masaru/trial_111.clj
+++ src/masaru/trial_111.clj
@@ -8,19 +8,37 @@
 
 (def STATES
   [{ 1 #{1}
-    :S #{2}}
+    :S 2}
    { 1 #{(P 2)}
     :$ #{(P 2)}}
    { 1 #{1}
     :$ #{(P 0)}
-    :S #{3}}
+    :S 3}
    { 1 #{1 (P 1)}
     :$ #{(P 1)}
-    :S #{3}}])
+    :S 3}])
 
 (defn sum-into [vs]
   (letfn [(get-numbers [v] (if (empty? (:datas v)) #{(:symbol v)} (:datas v)))
           (sum-up [ns ns'] (set (for [n ns n' ns'] (+ n n'))))]
     (reduce sum-up (map get-numbers vs))))
 
-(draw-forest-as-sexp (parse-forest STATES [1 1 1]))
+;; (draw-forest-as-sexp (parse-forest STATES [1 1 1]))
+
+(def consume' #(consume STATES %1 %2 (fn [dv] #{dv})))
+(def one1 (consume' 1 (->Vertex nil nil {0 #{}})))
+(def one2 (consume' 1 one1))
+(def one3 (consume' 1 one2))
+(def one4 (consume' 1 one3))
+(def one$ (consume' :$ one4))
+;; (clojure.pprint/pprint one$)
+(draw-forest-as-sexp one$)
+
+[(:S (:S (:S 1) (:S 1)) (:S (:S 1) (:S 1)))
+ (:S (:S 1) (:S (:S (:S 1) (:S 1)) (:S 1)))
+ (:S (:S (:S 1) (:S (:S 1) (:S 1))) (:S 1))
+ (:S (:S 1) (:S (:S 1) (:S (:S 1) (:S 1))))
+ (:S (:S (:S (:S 1) (:S 1)) (:S 1)) (:S 1))]
+
+[(:S (:S 1) (:S (:S 1) (:S 1)))
+ (:S (:S (:S 1) (:S 1)) (:S 1))]
