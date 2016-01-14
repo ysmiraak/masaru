1c15b530e853a2e34ca61c6c7fdfa42d27722e48
Ysmiraak <ysmiraak@gmail.com>
2016-01-14 17:33:22 +0100

    Better pars grammar.

--- src/masaru/trial/pars.clj
+++ src/masaru/trial/pars.clj
@@ -4,25 +4,47 @@
 (def P
   [ 0
    [:S]
-   [:S \( :S \) :S]])
+   [:S :S \( :S \)]])
 
 (def STATES
-  {0 {\( 2
+  {0 {\( (P 1)
       \) (P 1)
       :$ (P 1)
       :S 1}
-   1 {:$ (P 0)}
-   2 {\( 2
+   1 {\( 2
+      :$ (P 0)}
+   2 {\( (P 1)
       \) (P 1)
       :$ (P 1)
       :S 3}
+   3 {\( 2
+      \) 4}
+   4 {\( (P 2)
+      \) (P 2)
+      :$ (P 2)}})
+
+(def P'
+  [ 0
+   [:S]
+   [:S \( :S \) :S]])
+
+(def STATES'
+  {0 {\( 2
+      \) (P' 1)
+      :$ (P' 1)
+      :S 1}
+   1 {:$ (P' 0)}
+   2 {\( 2
+      \) (P' 1)
+      :$ (P' 1)
+      :S 3}
    3 {\) 4}
    4 {\( 2
-      \) (P 1)
-      :$ (P 1)
+      \) (P' 1)
+      :$ (P' 1)
       :S 5}
-   5 {\) (P 2)
-      :$ (P 2)}})
+   5 {\) (P' 2)
+      :$ (P' 2)}})
 
 (defn pairs
   "Generates all possible matchings of n pairs of parentheses."
@@ -41,6 +63,9 @@
 (-> (partial number-of-parses STATES)
     (map (pairs 8))
     set)                                ; #{1}
+(-> (partial number-of-parses STATES')
+    (map (pairs 8))
+    set)
 
 (defn count-pairs
   ([s] 1/2)
@@ -51,3 +76,5 @@
 (map (partial parse-for-result STATES count-pairs)
      (map #(apply str (repeat % "()")) (range 11)))
 ;; (0 1N 2N 3N 4N 5N 6N 7N 8N 9N 10N)
+(map (partial parse-for-result STATES' count-pairs)
+     (map #(apply str (repeat % "()")) (range 11)))
